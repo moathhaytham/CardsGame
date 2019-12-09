075833601e61fd3c1d59d2e3a5107f20df7f9283
moath <moath-moh@hotmail.com>
2019-12-09 19:42:15 +0200

    Animation, Layout, button locations all are set

--- app/src/main/java/com/example/cardsgame/MainActivity.java
+++ app/src/main/java/com/example/cardsgame/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
@@ -16,22 +18,39 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.example.cardsgame.R.drawable.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView t;
 
-    ArrayList imageviews = new ArrayList<ImageView>();
-    ArrayList cardOrientation;
+    int click=0;
+
+    int lastclicked;
+
+    ArrayList<ImageView> imageviews = new ArrayList<>();
+    ArrayList<String> cardOrientation = new ArrayList<>();
+
+    ArrayList<Integer> animals  = new ArrayList<>(Arrays.asList(R.drawable.bird , R.drawable.cat , R.drawable.dog ,
+            R.drawable.frog , R.drawable.monkey , R.drawable.mouse , R.drawable.penguin , R.drawable.bear ,
+            R.drawable.bird , R.drawable.cat , R.drawable.dog , R.drawable.frog , R.drawable.monkey , R.drawable.mouse ,
+            R.drawable.penguin , R.drawable.bear));
+
+    boolean canClick = true;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
 
-
-
+        //Randomize the animals images
+//        Collections.shuffle(animals);
 
         //Get ImageViews From the LinearLayout and add Action Listener
         LinearLayout linearLayout = findViewById(R.id.imagesWrapper);
@@ -60,7 +79,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     if (childView instanceof ImageView) {
                         childView.setOnClickListener(this);
                         childView.setId((4*i)+j);
-                        imageviews.add(childView);
+                        imageviews.add((ImageView)childView);
+                        cardOrientation.add("cardback");
                     }
                 }
             }
@@ -73,10 +93,148 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
-
-        int v = view.getId();
+        if(!canClick)
+            return;
+        
+        final int i = view.getId();
 
         final ImageView image = (ImageView) view;
+
+        if("cardback".equalsIgnoreCase(cardOrientation.get(i)) && click < 2){
+            canClick = false;
+
+            final ObjectAnimator oa1 = ObjectAnimator.ofFloat(image, "scaleX", 1f, 0f);
+            final ObjectAnimator oa2 = ObjectAnimator.ofFloat(image, "scaleX", 0f, 1f);
+            oa1.setInterpolator(new DecelerateInterpolator());
+            oa2.setInterpolator(new AccelerateDecelerateInterpolator());
+            oa1.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    image.setImageResource((Integer) animals.get(i));
+                    oa2.start();
+                }
+            });
+
+            oa2.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    click++;
+
+                    if(click == 1) {
+                        lastclicked = i;
+                        canClick = true;
+                    }
+
+                    if(click == 2)
+                    {
+                        click = 0;
+                        int x = animals.get(i);
+                        int y = animals.get(lastclicked);
+
+                        if(x == y) {
+//                            Toast.makeText(getApplicationContext(), "Match", Toast.LENGTH_LONG).show();
+                            image.setClickable(false);
+                            imageviews.get(lastclicked).setClickable(false);
+                            String audioName = getResources().getResourceName(x);
+                            audioName = audioName.split("/")[1];
+                            PlayAudio(audioName);
+                            //Toast.makeText(getApplicationContext(), getResources().getResourceName(x), Toast.LENGTH_LONG).show();
+                            canClick = true;
+                    }
+                        else
+                        {
+                            final ObjectAnimator oa1 = ObjectAnimator.ofFloat(image, "scaleX", 1f, 0f);
+                            final ObjectAnimator oa2 = ObjectAnimator.ofFloat(image, "scaleX", 0f, 1f);
+                            oa1.setInterpolator(new DecelerateInterpolator());
+                            oa2.setInterpolator(new AccelerateDecelerateInterpolator());
+                            oa1.addListener(new AnimatorListenerAdapter() {
+                                @Override
+                                public void onAnimationEnd(Animator animation) {
+                                    super.onAnimationEnd(animation);
+                                    image.setImageResource(R.drawable.pawprint);
+                                    oa2.start();
+                                }
+                            });
+                            oa1.start();
+
+                            final ObjectAnimator oa3 = ObjectAnimator.ofFloat(imageviews.get(lastclicked), "scaleX", 1f, 0f);
+                            final ObjectAnimator oa4 = ObjectAnimator.ofFloat(imageviews.get(lastclicked), "scaleX", 0f, 1f);
+                            oa3.setInterpolator(new DecelerateInterpolator());
+                            oa4.setInterpolator(new AccelerateDecelerateInterpolator());
+                            oa3.addListener(new AnimatorListenerAdapter() {
+                                @Override
+                                public void onAnimationEnd(Animator animation) {
+                                    super.onAnimationEnd(animation);
+                                    imageviews.get(lastclicked).setImageResource(R.drawable.pawprint);
+                                    oa4.start();
+                                }
+                            });
+
+                            oa4.addListener(new AnimatorListenerAdapter() {
+                                @Override
+                                public void onAnimationEnd(Animator animation) {
+                                    super.onAnimationEnd(animation);
+                                    canClick = true;
+                                }
+                            });
+
+                            oa3.start();
+                        }
+                    }
+                }
+            });
+
+            oa1.start();
+
+//            click++;
+
+//            if(click ==1)
+//                lastclicked = i;
+        }
+
+        else if(!"cardback".equalsIgnoreCase(cardOrientation.get(i))){
+
+        }
+
+        if(click == 2)
+        {
+//            if(image.getId() == imageviews.get(lastclicked).getId())
+//                Toast.makeText(getApplicationContext(), "Match", Toast.LENGTH_LONG).show();
+//
+//            else
+//            {
+//                final ObjectAnimator oa1 = ObjectAnimator.ofFloat(image, "scaleX", 1f, 0f);
+//                final ObjectAnimator oa2 = ObjectAnimator.ofFloat(image, "scaleX", 0f, 1f);
+//                oa1.setInterpolator(new DecelerateInterpolator());
+//                oa2.setInterpolator(new AccelerateDecelerateInterpolator());
+//                oa1.addListener(new AnimatorListenerAdapter() {
+//                    @Override
+//                    public void onAnimationEnd(Animator animation) {
+//                        super.onAnimationEnd(animation);
+//                        image.setImageResource(R.drawable.pawprint);
+//                        oa2.start();
+//                    }
+//                });
+//                oa1.start();
+//
+//                final ObjectAnimator oa3 = ObjectAnimator.ofFloat(imageviews.get(lastclicked), "scaleX", 1f, 0f);
+//                final ObjectAnimator oa4 = ObjectAnimator.ofFloat(imageviews.get(lastclicked), "scaleX", 0f, 1f);
+//                oa1.setInterpolator(new DecelerateInterpolator());
+//                oa2.setInterpolator(new AccelerateDecelerateInterpolator());
+//                oa1.addListener(new AnimatorListenerAdapter() {
+//                    @Override
+//                    public void onAnimationEnd(Animator animation) {
+//                        super.onAnimationEnd(animation);
+//                        imageviews.get(lastclicked).setImageResource(R.drawable.pawprint);
+//                        oa2.start();
+//                    }
+//                });
+//                oa1.start();
+//            }
+
+        }
         //String T = Integer.toString(v);
         //Toast.makeText(getApplicationContext(), T, Toast.LENGTH_LONG).show();
 
@@ -95,20 +253,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         flip.start();
         */
 
-        final ObjectAnimator oa1 = ObjectAnimator.ofFloat(image, "scaleX", 1f, 0f);
-        final ObjectAnimator oa2 = ObjectAnimator.ofFloat(image, "scaleX", 0f, 1f);
-        oa1.setInterpolator(new DecelerateInterpolator());
-        oa2.setInterpolator(new AccelerateDecelerateInterpolator());
-        oa1.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                image.setImageResource(R.drawable.fox);
-                oa2.start();
-            }
-        });
-        oa1.start();
 
 
+
+    }
+
+    public void PlayAudio(String name){
+        int ID = getResources().getIdentifier(name, "raw", getPackageName());
+        MediaPlayer audio = MediaPlayer.create(this ,ID);
+        audio.setLooping(false);
+        audio.setVolume(100, 100);
+        audio.start();
     }
 }
